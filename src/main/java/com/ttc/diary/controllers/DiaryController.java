0210988faaa8d9f33e7d444ed201c70ab86d9dd0
quangvm <vuminhquang0598@gmail.com>
2021-05-10 10:04:45 +0700

    update create-diary feature

--- src/main/java/com/ttc/diary/controllers/DiaryController.java
+++ src/main/java/com/ttc/diary/controllers/DiaryController.java
@@ -3,11 +3,12 @@ package com.ttc.diary.controllers;
 import com.ttc.diary.models.DiaryDto;
 import com.ttc.diary.services.DiaryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin("*")
 @RequestMapping("api/v1/diaries")
+@PreAuthorize("isAuthenticated()")
 public class DiaryController {
 
     private final DiaryService diaryService;
