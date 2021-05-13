6aafe60477c4266d2bf80dc1007f6b65c489ae03
quangvm <vuminhquang0598@gmail.com>
2021-05-13 04:01:23 -0500

    feature/create-topic

--- src/main/java/com/ttc/diary/repository/TopicRepository.java
+++ src/main/java/com/ttc/diary/repository/TopicRepository.java
@@ -4,6 +4,12 @@ import com.ttc.diary.model.entity.Topic;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TopicRepository extends JpaRepository<Topic, Long> {
+
+    Optional<Topic> findByName(String name);
+
+    boolean existsByName(String name);
 }
--- src/main/java/com/ttc/diary/service/TopicService.java
+++ src/main/java/com/ttc/diary/service/TopicService.java
@@ -1,9 +1,12 @@
 package com.ttc.diary.service;
 
 import com.ttc.diary.model.dto.TopicDto;
+import com.ttc.diary.model.response.BaseResponse;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 public interface TopicService {
     public List<TopicDto> getAllTopics();
+    ResponseEntity<BaseResponse<TopicDto>> createTopic(TopicDto dto);
 }
